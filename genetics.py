e444dfb90fdd0431210fb088e5b8dccbbc141708
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2020-05-10 01:42:34 -0400

    Return none if there is no dataframes

--- genetics.py
+++ genetics.py
@@ -10,47 +10,47 @@ import pandas as pd
 class Genetics:
     url = 'http://genetics-api.opentargets.io/graphql'
     endpoint = HTTPEndpoint(url)
-        
+
     def genes_for_variant(self, variants, data_frame=True):
         op = Operation(Query)
 
         for variant in variants:
             op.genes_for_variant(variant_id=variant, __alias__='alias_' + variant).__fields__()
         data = self.endpoint(op)
-        
+
         if data_frame:
             return json_normalize([alias for aliases in data['data'].values() for alias in aliases], sep='_')
         else:
             result = (op+data)
             return {x: getattr(result, f'alias_{x}') for x in variants}
-        
+
     def study_info(self, study_ids, data_frame=True):
         op = Operation(Query)
-        
+
         for study_id in study_ids:
             s = op.study_info(study_id=study_id, __alias__='alias_' + study_id)
             s.__fields__()
-            
+
         data = self.endpoint(op)
         if data_frame:
             return pd.json_normalize(data['data'].values(), sep='_')
         else:
             result = (op+data)
-            return {x: getattr(result, f'alias_{x}') for x in study_ids}        
-        
+            return {x: getattr(result, f'alias_{x}') for x in study_ids}
+
     def manhattan(self, study_ids, data_frame=True):
         op = Operation(Query)
-        
+
         for study_id in study_ids:
             m = op.manhattan(study_id=study_id, __alias__='alias_' + study_id)
-            
+
             m.associations.best_genes.gene.id()
             m.associations.best_genes.gene.symbol()
             m.associations.best_coloc_genes.gene.id()
             m.associations.best_coloc_genes.gene.symbol()
             m.associations.best_locus2_genes.gene.id()
             m.associations.best_locus2_genes.gene.symbol()
-            
+
             m.associations().__fields__()
             m.associations().variant().id()
 
@@ -60,7 +60,8 @@ class Genetics:
             for study, aliases in data['data'].items():
                 for alias in aliases['associations']:
                     dfs.append(json_normalize(alias, sep='_').assign(study=study.split('alias_')[1]))
-            return pd.concat(dfs, axis=0).reset_index(drop=True)
+            if dfs:
+                return pd.concat(dfs, axis=0).reset_index(drop=True)
         else:
             result = (op+data)
             return {x: getattr(result, f'alias_{x}') for x in study_ids}
